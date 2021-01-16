fd4fc331d7433e25ab4385d5d65b28cf02f1a96f
samradh3011 <77258318+samradh3011@users.noreply.github.com>
2021-01-16 13:26:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,18 @@ import streamlit as st
 import pandas as pd
 
 
-st.set_page_config(page_icon='📅', page_title='Your Data Frame')
+st.set_page_config(page_icon='📅', page_title='Data Viewer')
 st.title('**Read, Write and Understand Data**')
 data = st.file_uploader('Browse files to Open', type=['csv','xlsx'])
-if data is None:
-    pass
-else:
-    data = pd.read_csv(data)
-    st.write('Your Dataframe:')
-    st.dataframe(data)
-    st.write('*Number of Columns*: ', len(data.columns))
-    st.write('Null Values:', data.isna().sum())
-    st.write('Summary:', data.describe())
+try:
+    if data is None:
+        pass
+    else:
+        data = pd.read_csv(data)
+        st.write('Your Dataframe:')
+        st.dataframe(data)
+        st.write('*Number of Columns*: ', len(data.columns))
+        st.write('Null Values:', data.isna().sum())
+        st.write('Summary:', data.describe())
+except:
+    st.write('Error')
