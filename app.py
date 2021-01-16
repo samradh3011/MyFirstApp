cb7dab7c82fc59318355d25c91d337364abb35de
samradh3011 <77258318+samradh3011@users.noreply.github.com>
2021-01-16 13:37:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,14 @@ import pandas as pd
 st.set_page_config(page_icon='📅', page_title='Data Viewer')
 st.title('**Read, Write and Understand Data**')
 data = st.file_uploader('Browse files to Open', type=['csv','xlsx'])
-try:
-    if data is None:
-        pass
-    else:
-        data = pd.read_csv(data)
-        st.write('Your Dataframe:')
-        st.dataframe(data)
-        st.write('*Number of Columns*: ', len(data.columns))
-        st.write('Null Values:', data.isna().sum())
-        st.write('Summary:', data.describe())
-except:
-    st.write('**Error while loading Dataset**')
+
+if data is None:
+    pass
+else:
+    data = pd.read_csv(data)
+    st.write('Your Dataframe:')
+    st.dataframe(data)
+    st.write('*Number of Columns*: ', len(data.columns))
+    st.write('Null Values:', data.isna().sum())
+    st.write('Summary:', data.describe())
+
