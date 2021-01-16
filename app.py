974c50782576a0b81c85420ead2a379185ef524f
samradh3011 <77258318+samradh3011@users.noreply.github.com>
2021-01-16 11:44:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-import pandas as pd
-import numpy as np
 import streamlit as st
+import pandas as pd
 
-data = pd.read_csv('C:\\Users\\HP DB0186AU\\Desktop\\Data_Science\\Assignments\\Multiple Linear Regression\\Multiple Linear Regression\\Cars.csv')
-uploader = st.file_uploader('browse file')
-st.dataframe(uploader)
 
+
+st.title('**Read, Write and Understand Data**')
+data = st.file_uploader('Browse files to Open', type=['csv','xlsx'])
+if data is None:
+    pass
+else:
+    data = pd.read_csv(data)
+    st.write('Your Dataframe:')
+    st.dataframe(data)
+    st.write('*Number of Columns*: ', len(data.columns))
+    st.write('Null Values:', data.isna().sum())
+    st.write('Summary:', data.describe())
