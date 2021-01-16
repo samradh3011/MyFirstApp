39759eff5e578a42142432e20d51e8668a2bd758
samradh3011 <77258318+samradh3011@users.noreply.github.com>
2021-01-16 11:36:01 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb
+import streamlit as st
+
+data = pd.read_csv('C:\\Users\\HP DB0186AU\\Desktop\\Data_Science\\Assignments\\Multiple Linear Regression\\Multiple Linear Regression\\Cars.csv')
+uploader = st.file_uploader('browse file')
+st.dataframe(uploader)
+
