5d59e5e4fa4957b81fe02d18fbfa1bc529ca5112
samradh3011 <77258318+samradh3011@users.noreply.github.com>
2021-01-16 13:24:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 
-st.set_page_config(page_icon='📖', page_title='Your Data Frame')
+st.set_page_config(page_icon='📅', page_title='Your Data Frame')
 st.title('**Read, Write and Understand Data**')
 data = st.file_uploader('Browse files to Open', type=['csv','xlsx'])
 if data is None:
